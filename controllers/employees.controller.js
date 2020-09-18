6736e16a90e65d4c327be8bd208962a57f51a7af
Bartosz Glinka <bartosz.glinka@hotmail.com--global>
2020-09-19 00:05:47 +0200

    add controllers to project

---
+++ controllers/employees.controller.js
@@ -0,0 +1,85 @@
+const Employee = require('../routes/models/employee.model');
+
+exports.getAll = async (req, res) => {
+    try {
+      res.json(await Employee.find().populate('idDepartment'));
+    }
+    catch(err) {
+      res.status(500).json({ message: err });
+    }
+};
+
+exports.getRandom = async (req, res) => {
+
+    try {
+      const count = await Employee.countDocuments();
+      const rand = Math.floor(Math.random() * count);
+      const dep = await Employee.findOne().skip(rand);
+      if(!dep) res.status(404).json({ message: 'Not found' });
+      else res.json(dep);
+    }
+    catch(err) {
+      res.status(500).json({ message: err });
+    }
+  
+};
+
+exports.getOne = async (req, res) => {
+
+    try {
+      const dep = await Employee.findById(req.params.id);
+      if(!dep) res.status(404).json({ message: 'Not found' });
+      else res.json(dep);
+    }
+    catch(err) {
+      res.status(500).json({ message: err });
+    }
+  
+};
+
+exports.addNew = async (req, res) => {
+
+    try {
+  
+      const { firstName, lastName, idDepartment, salary   } = req.body;
+      const newEmployee = new Employee({ firstName: firstName, lastName: lastName, idDepartment: idDepartment, salary: salary  });
+      await newEmployee.save();
+      res.json({ message: 'OK' });
+  
+    } catch(err) {
+      res.status(500).json({ message: err });
+    }
+  
+};
+
+exports.updateOne = async (req, res) => {
+    const { firstName, lastName, idDepartment, salary   } = req.body;
+    try {
+      const dep = await(Employee.findById(req.params.id));
+      if(dep) {
+        await Employee.updateOne({ _id: req.params.id }, { $set: { firstName: firstName, lastName: lastName, idDepartment: idDepartment, salary: salary }});
+        res.json({ message: 'OK' });
+      }
+      else res.status(404).json({ message: 'Not found...' });
+    }
+    catch(err) {
+      res.status(500).json({ message: err });
+    }
+  
+};
+
+exports.deleteOne = async (req, res) => {
+
+    try {
+      const dep = await(Employee.findById(req.params.id));
+      if(dep) {
+        await Employee.deleteOne({ _id: req.params.id });
+        res.json({ message: 'OK' });
+      }
+      else res.status(404).json({ message: 'Not found...' });
+    }
+    catch(err) {
+      res.status(500).json({ message: err });
+    }
+  
+};
--- routes/models/employee.model.js
+++ routes/models/employee.model.js
@@ -1,9 +1,10 @@
+const { ObjectID } = require('mongodb');
 const mongoose = require('mongoose');
 
 const employeeSchema = new mongoose.Schema({
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
-    department: { type: String, required: true },
+    idDepartment: { type: String, required: true, ref: 'Department' },
     salary: { type: Number, required: true }
 });
 
